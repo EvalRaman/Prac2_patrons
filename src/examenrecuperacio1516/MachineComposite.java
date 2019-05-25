85de04ce8b45a35f16a68ccfbac34cd5097f054b
Xavi <xavierllubes11@gmail.com>
2019-05-25 19:31:53 +0200

    Apartat c

--- src/examenrecuperacio1516/MachineComposite.java
+++ src/examenrecuperacio1516/MachineComposite.java
@@ -2,26 +2,63 @@ package examenrecuperacio1516;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observer;
 
-public class MachineComposite extends MachineComponent {
+public class MachineComposite extends MachineComponent implements Observer {
     private List<MachineComponent> components = new ArrayList<>();
+    private List<MachineComponent> brokenComponents = new ArrayList<>();
+
 
     public void addComponent(MachineComponent mc) {
         components.add(mc);
+        mc.addObserver(this);
+    }
 
+    public void setBroken() {
+        if (!isBroken()) {
+            broken = true;
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    public void repair() {
+        if (isBroken()) {
+            for (MachineComponent component : brokenComponents) {
+                component.repair();
+                brokenComponents.remove(component);
+            }
+            broken = false;
+            setChanged();
+            notifyObservers();
+        }
     }
 
     public boolean isBroken() {
-        if (broken) {
-            return true;
-        }
-        for (MachineComponent mc : components) {
-            if (mc.isBroken()) {
-                return true;
-            }
-        }
-        return false;
-
+        return broken;
     }
 
+    @Override
+    public void update(java.util.Observable o, Object arg) {
+        MachineComponent mc = (MachineComponent) arg;
+        if (mc.isBroken()) {
+            if (!brokenComponents.contains(mc))
+                brokenComponents.add(mc);
+            if (!broken) {
+                broken = true;
+                setChanged();
+                notifyObservers();
+            }
+
+        } else if (!mc.isBroken()) {
+            brokenComponents.remove(mc);
+            if (brokenComponents.isEmpty() && broken) {
+                broken = false;
+                setChanged();
+                notifyObservers();
+            }
+
+        }
+
+    }
 }
